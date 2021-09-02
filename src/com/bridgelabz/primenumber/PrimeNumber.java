13faf3484b2867becfa37c5315e4dfdf4c3ec095
Swathi <swathi.h@perfios.com>
2021-09-02 19:49:02 +0530

    ADDED: Method to print prime numbers between 1 to 1000

---
+++ src/com/bridgelabz/primenumber/PrimeNumber.java
@@ -0,0 +1,29 @@
+package com.bridgelabz.primenumber;
+
+public class PrimeNumber {
+
+	private static String primeNumbers() {
+		String primeNumbers = "";
+
+		for (int number = 3; number <= 1000; number++) {
+			int encounter = 0;
+
+			for (int index = number; index >= 1; index--) {
+				if (number % index == 0) {
+					encounter = encounter + 1;
+				}
+			}
+
+			if (encounter == 2) {
+				primeNumbers = primeNumbers + number + " ";
+			}
+		}
+		return primeNumbers;
+	}
+
+	public static void main(String[] args) {
+		String primeNumber = primeNumbers();
+		System.out.println("Prime numbers between 1 to 1000:");
+		System.out.println(primeNumber);
+	}
+}
